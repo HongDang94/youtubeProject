0a39e42653e8325aeb8b464ba273f5baa58caf02
hong.dt <hong.thdang@gmail.com>
2022-07-04 23:41:39 +0700

    update

--- src/components/Youtube/YoutubeList.tsx
+++ src/components/Youtube/YoutubeList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import YoutubeItem from './YoutubeItem';
 
 const DefaultListYoutube = [
@@ -11,11 +12,40 @@ const DefaultListYoutube = [
     id: 2,
     name: "kenh youtube 2",
     imgUrl: "https://picsum.photos/168/94",
-    author: "qdhhdh",
+    author: "hong hihi",
   }
 ]
 
 function YoutubeList() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://api.example.com/items")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
   return (
     <div>
       {DefaultListYoutube.map((item) => {
@@ -25,6 +55,7 @@ function YoutubeList() {
       
     </div>
   );
+  }
 }
 
 export default YoutubeList;
